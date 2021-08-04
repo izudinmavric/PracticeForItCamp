a4a49587abdfed025cea55871f6198c11f61a13d
mac <izudin.mavric@gmail.com>
2021-08-05 00:26:47 +0200

    Function

--- vezba16js/file.js
+++ vezba16js/file.js
@@ -138,13 +138,18 @@
 // console.log(suma-zbir)
 
 
-for(var i=5;i>=1;i--) {
-    console.log("\n");
-
-    for(var j=i;j>=1;j--){
-        var s = "*";
-        s===j;
-        console.log(s);
-    }
-        // console.log("\n");
+let n = 5; 
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  // printing star
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
 }
+console.log(string);
---
+++ vezba17js/file.js
@@ -0,0 +1,103 @@
+// function name(a,b,c,d,e){
+//     console.log(a,b,c,d,e)
+// }
+
+// name(1,2,3,4,5);
+// name(1,2,3,4,5);
+// name();
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// const add1 = add(1,2);
+// console.log(add1)
+
+
+// function max(a,b,c){
+
+//     let max;
+//     max = a;
+
+//     if(max>b){
+//         max = b;
+//     }else if(max<c){
+//         max = c;
+//     }
+// return max
+// }
+
+
+// console.log(max(10,43,54);
+
+
+function counter(string){  
+    let counter = 0;
+
+    for(var i = 0; i<string.length; i++){
+        if(string[i] === 'a' || string[i] === 'i' || string[i] === 'o' || string[i] === 'e' || string[i] === 'u'){
+            counter ++;
+        }
+    }
+
+    return counter;
+
+}
+
+function maxCounter (str1, str2, str3, str4, str5){
+
+
+    var result = str1;
+    var max = counter(str1)
+    var array = [str2,str3,str4,str5];
+    for(var i= 0; i<array.length;i++){
+
+        if (counter(array[i] > max)){
+            max = counter[array[i]]
+            result = array[i];
+
+        }
+
+    }
+
+
+    // var str1c = counter(str1);
+    // var str2c = counter(str2);
+    // var str3c = counter(str3);
+    // var str4c = counter(str4);
+    // var str5c = counter(str5);
+
+    // var max = str1c;
+    // var result = str1;
+
+    //     if(max<str2c){
+    //         max = str2c;
+    //         result = str2;
+    //     }
+
+    //     if(max<str3c){
+    //         max = str3c;
+    //         result = str3;
+    //     }
+
+    //     if(max<str4c){
+    //         max = str4c;
+    //         result = str4;
+
+    //     }
+
+    //     if(max<str5c){
+    //         max = str5c;
+    //         result = str5;
+
+    //     }
+
+        return result;
+
+}
+
+
+
+console.log(maxCounter("Gdgdfgfggfdg", "sdassaffdfs","fdsfdagafddfgfdfdgd","fgdaggfadgfdagadfg","gfdgadfgfgdag"));
+
+
